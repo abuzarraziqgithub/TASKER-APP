758b8111eabd5e4cd1988ca031db9dddc09dbb28
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-04-29 00:13:15 +0500

    script-added

--- do.js
+++ do.js
@@ -1,1 +1,12 @@
 "use strict";
+// DOCUMENT ELEMENTS
+const inputData = document.getElementById("toDoInput");
+const addBtn = document.getElementById("addBtn");
+const listingData = document.querySelector(".list-data");
+
+// ul > li > h3.innerHTML
+
+const addTask = function () {
+  const ul = document.createElement("ul");
+  ul.classList.add("ul-list");
+};
