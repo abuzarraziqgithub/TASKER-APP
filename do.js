fce2b8d35014f9466e9b5083a8625d02f786afe8
Abuzar RaziQ <abuzarraziqo@gmail.com>
2024-05-01 16:22:38 +0500

    LOGIC-CHANGES

--- do.js
+++ do.js
@@ -4,9 +4,30 @@ const inputData = document.getElementById("toDoInput");
 const addBtn = document.getElementById("addBtn");
 const listingData = document.querySelector(".list-data");
 
-// ul > li > h3.innerHTML
+// CREATING HTML ELEMENTS:
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+const h3 = document.createElement("h3");
 
+// ADDING TASK FUNCTION
 const addTask = function () {
-  const ul = document.createElement("ul");
   ul.classList.add("ul-list");
+  ul.appendChild(li);
+  li.appendChild(h3);
+  h3.innerText = inputData.value;
+  listingData.appendChild(ul);
+
+  inputData.value = "";
 };
+
+h3.style.cursor = "pointer";
+addBtn.addEventListener("click", addTask);
+
+h3.addEventListener("dblclick", function () {
+  h3.style.textDecoration = "line-through";
+
+  setTimeout(function () {
+    li.style.listStyle = "none";
+    h3.innerText = "";
+  }, 1000);
+});
